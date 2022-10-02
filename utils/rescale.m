23730e09d85f3517fc39df8cad052225851f80ae
Tianyu Dai <dai.ti@husky.neu.edu>
2022-10-02 15:25:52 -0400

    Delete utils directory

--- utils/rescale.m
+++
@@ -1,9 +0,0 @@
-function sys2 = rescale(sys)
-% rescale such that the coefficient of highest order term = 1
-[b,a] = tfdata(sys);
-b = b{1};
-a = a{1};
-b = b/a(1);
-a = a/a(1);
-sys2 = tf(b,a,0.1);
-end
